667ff58258c4ce68b6074426d4811e5146a78028
BoAi-GIS <aibogis@163.com>
2019-05-05 16:24:36 +0800

    Renaming work mainly includes: 1、Modify assembly name 2、Rename namespaces

--- GosperCoding/Properties/AssemblyInfo.cs
+++ GosperCoding/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Gosper索引实验")]
+[assembly: AssemblyTitle("GosperCoding")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gosper索引实验")]
+[assembly: AssemblyProduct("GosperCoding")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
